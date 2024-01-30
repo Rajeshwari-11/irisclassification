a4e19d22505f7b0ffb5e3ecedf25b11989a4608c
Rajeshwari-11 <gunagantirajeshwari1@gmail.com>
2024-01-30 11:04:37 +0530

    added sklearn

--- app.py
+++ app.py
@@ -1,28 +1,52 @@
 import streamlit as st
-from keras.models import load_model 
-import numpy as np 
+from keras.models import load_model
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
 
-model = load_model("model.h5")
-labels = np.load("labels.npy") 
+# Load and preprocess data using scikit-learn
+def load_and_preprocess_data():
+    data = pd.read_csv('iris.data', header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+
+    # Encoding labels with scikit-learn LabelEncoder
+    label_encoder = LabelEncoder()
+    data['class'] = label_encoder.fit_transform(data['class'])
+
+    X = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']].values
+    y = to_categorical(data['class'])
+
+    return X, y, label_encoder.classes_
 
 st.title("Welcome to flower prediction app")
+
+# Load the model and labels
+model = load_model("model.h5")
+labels = np.load("labels.npy")
+
+# Load and preprocess data using scikit-learn
+X, y, classes = load_and_preprocess_data()
+
+# Split the data into training and testing sets using scikit-learn
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 a = float(st.number_input("sepal length in cm"))
 b = float(st.number_input("sepal width in cm"))
 c = float(st.number_input("petal length in cm"))
 d = float(st.number_input("petal width in cm"))
 
-btn = st.button("predict")
+btn = st.button("Predict")
 
 if btn:
-	pred = model.predict(np.array([a,b,c,d]).reshape(1,-1))
-	pred = labels[np.argmax(pred)]
-	st.subheader(pred)
+    # Use the model to make predictions
+    pred = model.predict(np.array([a, b, c, d]).reshape(1, -1))
 
-	if pred=="Iris Setosa":
-		st.image("setosa.jpg")
-	elif pred=="Iris Versicolour":
-		st.image("versicolor.jpg")
-	else:	
-		st.image("verginca.jpg")
+    # Get the predicted label
+    pred_label = labels[np.argmax(pred)]
+    st.subheader(f"Predicted Label: {pred_label}")
 
+    # Convert the predicted label to the corresponding class name
+    predicted_class_name = classes[pred_label]
+
+    # Display the corresponding image
+    st.image(f"{predicted_class_name.lower()}.jpg", caption=f"{predicted_class_name} Iris")
--- model_train.py
+++ model_train.py
@@ -1,66 +1,33 @@
-from keras.models import Model 
-from keras.layers import Input, Dense  
-import pandas as pd 
+from keras.models import Model
+from keras.layers import Input, Dense
+import pandas as pd
 from tensorflow.keras.utils import to_categorical
 import numpy as np
+from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('iris.data').values
+data = pd.read_csv('iris.data', header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 
-rand = np.arange(149)
-np.random.shuffle(rand)
-c = 0
+# Encoding labels with scikit-learn LabelEncoder
+label_encoder = LabelEncoder()
+data['class'] = label_encoder.fit_transform(data['class'])
 
-copy = data.copy()
+X = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']].values
+y = to_categorical(data['class'])
 
-for i in rand:
-	data[c] = copy[i]
-	c = c+1
-
-print(data)
-
-X = data[:, :4]
-y = data[:, 4]
-print(y)
-labels = {}
-cnt = 0 
-
-for i in y:
-	if i not in labels:
-		labels[i] = cnt  
-		cnt = cnt + 1 
-
-print("="*50)
-print(labels)
-for i in range(y.shape[0]):
-	y[i] = labels[y[i]]
-print(y)
-y = to_categorical(y)
-print(y)
-
-X= np.array(X, dtype="float64")
-y= np.array(y, dtype="float64")
-
-print(X.shape, y.shape)
+# Split the data into training and testing sets using scikit-learn
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 inp = Input(shape=(4))
-
 x = Dense(32, activation="relu")(inp)
-
 op = Dense(3, activation="softmax")(x)
 
-model=  Model(inputs=inp, outputs=op)
+model = Model(inputs=inp, outputs=op)
 
 model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=['acc'])
-
-model.fit(X, y, epochs=30)
+model.fit(X_train, y_train, epochs=30)
 
 model.save('model.h5')
 
-arr = []
+arr = label_encoder.classes_
 
-for k in labels.keys():
-	arr.append(k)
-	
-print(arr)
-print(labels)
-np.save("labels.npy", np.array(arr))
+np.save("labels.npy", arr)
